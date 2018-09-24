57d9e7225f472f1a48c8da279d275ce45a367538
khffaizmohammadi@gmail.com <khaterahFaizmohammadi>
2018-09-24 03:40:51 -0700

    first commit

---
+++ Assets/Script/KeyBoardCtrl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class KeyBoardCtrl : MonoBehaviour {
+
+	public string inputText = "text";
+	private TouchScreenKeyboard keyboard;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI(){
+		if (GUI.Button (new Rect (0, 10, 200, 32), inputText))
+			keyboard = TouchScreenKeyboard.Open (inputText);
+
+		if (keyboard != null)
+			inputText = keyboard.text;
+	}
+
+	}
---
+++ Assets/Script/SceneCtrl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneCtrl : MonoBehaviour {
+
+	public void Load (string n){
+
+		SceneManager.LoadScene (n);
+	}
+}
